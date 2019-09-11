f99ca9445ee55dfdcba414aa0489e0e3bf04dd93
Suebersson <43149447+Suebersson@users.noreply.github.com>
2019-09-11 14:24:59 -0300

    Update testeJs.js

--- testeJs.js
+++ testeJs.js
@@ -1,1 +1,11 @@
 alert('teste Github');
+
+/*
+var GitHub = document.createElement("script");
+GitHub.id = '_gitHub';
+GitHub.type = 'text/javascript';
+//GitHub.async = true; //Executar script junto com o carregamento da página
+GitHub.src = "https://Suebersson.github.io/DisparadorWhatsApp/testeJs.js";
+GitHub.onload = function() {this.parentNode.removeChild(this);}
+document.body.appendChild(GitHub); //será carregado no body da página
+*/
