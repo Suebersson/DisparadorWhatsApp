ae84121c1cfc9fe4c4931c69f1c489cd03171637
Suebersson <43149447+Suebersson@users.noreply.github.com>
2019-12-02 00:50:14 -0300

    Add files via upload

---
+++ ExtrairMembrosGrupo.js
@@ -0,0 +1,106 @@
+
+//########################## Atualizado em 11/11/2019 ######################################################################
+
+var ListaContatos = new Array(), Nacional, num, j = 0, obj;
+
+function ExtrairMembrosGrupo(){
+
+	console.clear();
+
+	if (window.Store){
+		//Ler o grupo ativo, ler os contatos do grupo e imprimir
+		if(Store.Chat.active().__x_isGroup == true){
+			
+			console.log('Chat_nome: ' + Store.Chat.active().__x_formattedTitle);
+			console.log('Chat_server: ' + Store.Chat.active().__x_id.server);
+			console.log('Chat_user: ' + Store.Chat.active().__x_id.user);
+			console.log('Chat_Id: ' + Store.Chat.active().__x_id._serialized);
+			
+			Store.GroupMetadata.find(Store.Chat.active().__x_id._serialized).then(function(g) {
+				
+				obj = g.participants._models;
+				
+				console.log('Total de participantes: ' + obj.length);
+				
+				if(obj.length > 2){
+					
+					//if (confirm("Deseja capturar apenas números nacionais?") == true){Nacional = 1;}else{Nacional = 0;}
+					
+					//https://sweetalert.js.org/
+					swal("Deseja capturar apenas números nacionais?", {
+						buttons: {
+							Yes: "Sim",
+							No: "Não"
+						},
+					}).then((value) => {
+						switch (value) {
+							case "Yes":
+								Nacional = 1
+								get_members()
+								break;
+							case "No":
+								Nacional = 0
+								get_members()
+								break;
+							default:
+							//Não fazer nada
+						}
+					});
+	
+					function get_members(){
+						for (q = 0; q < obj.length; q++){
+							
+							if(Store.Conn.__x_me.user == obj[q].__x_id.user){
+								//Excluir o número da conta logada que também participa do grupo
+							}else{
+								//console.log(obj[q].__x_id.user);
+								num = obj[q].__x_id.user
+								
+								if(Nacional == 1){
+									if(num.slice(0, 2) == "55"){//remover o código 55
+										ListaContatos[j] = num.slice(2, num.length)
+										j++
+									}
+								}else{//Capturar todos os números, nacionais e internacionais
+								
+									if(num.slice(0, 2) == "55"){//remover o código 55
+										ListaContatos[j] = num.slice(2, num.length)
+										j++
+									}else{
+										ListaContatos[j] = num
+										j++
+									}
+									
+								}
+							}
+						}
+						
+						var fileNameToSaveAs = prompt('Digite ou verifique o nome do grupo (Excluindo emojis):', Store.Chat.active().__x_formattedTitle) + '.txt'; //Nome do txt
+
+						if(fileNameToSaveAs != '' && fileNameToSaveAs != 'null.txt'){
+		
+							printTxt(ListaContatos, fileNameToSaveAs)
+							ListaContatos = new Array()
+						
+						}else{
+							alert("Nome do arquivo não inserido")
+						}
+		
+					}
+				
+				}else{
+					alert('Nesse grupo não à participantes suficiente para extrair os contatos')
+				}
+			});
+			
+		}else{
+			alert("O Chat selecionado não é um grupo")
+		}
+
+	}else{
+		console.warn('É necessário fazer manutenção no código fonte. "Store" não definida')
+	}
+
+}
+
+
