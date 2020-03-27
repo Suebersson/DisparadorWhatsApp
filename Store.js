441a63888d1f0bc1eaffb47bd7229056bc89de56
Suebersson <43149447+Suebersson@users.noreply.github.com>
2020-03-27 15:25:07 -0300

    Update Store.js

--- Store.js
+++ Store.js
@@ -99,7 +99,7 @@ if (!window.Store) {
 			}
 		}
 
-		webpackJsonp([], { 'parasite': (x, y, z) => getStore(z) }, ['parasite']);
+		//webpackJsonp([], { 'parasite': (x, y, z) => getStore(z) }, ['parasite']);
 	})();
 }
 
