07cd1929b37943904238b8337f847fb5763863f1
Suebersson Montalvão <43149447+Suebersson@users.noreply.github.com>
2021-11-22 21:37:09 -0300

    Update Status_bateria.js

--- Status_bateria.js
+++ Status_bateria.js
@@ -6,6 +6,48 @@ setInterval(Atualizar_Status, 120000);
 function Atualizar_Status(){
 	
 	if (window.Store){
+
+		if (class_Name == undefined){
+			for (q=0;q<document.querySelectorAll("div").length;q++){
+				
+				if (document['getElementsByTagName']('div')[q]['innerHTML'] == 'Pesquisar ou começar uma nova conversa'
+				|| document['getElementsByTagName']('div')[q]['innerHTML'] == 'Buscar o empezar un chat nuevo'
+				|| document['getElementsByTagName']('div')[q]['innerHTML'] == 'Search or start new chat'){
+				
+					class_Name = document['getElementsByTagName']('div')[q].className
+					
+					document['getElementsByClassName'](class_Name)[0]['innerHTML'] = Store.Me.battery != undefined 
+						? 'Status da Bateria: ' + Store.Me.battery + '%'
+						: 'Extensão sincronizada na versão beta'
+					//console.log('Status da Bateria: ' + Store.Me.battery + '%');
+					break;
+				}
+			}
+		}else{
+			if (document['getElementsByClassName'](class_Name)[0]){
+				document['getElementsByClassName'](class_Name)[0]['innerHTML'] = Store.Me.battery != undefined 
+					? 'Status da Bateria: ' + Store.Me.battery + '%'
+					: 'Extensão sincronizada na versão beta'
+				//console.log('Status da Bateria: ' + Store.Me.battery + '%')
+			}
+		}
+		
+	}else{
+		console.log('É necessário fazer manutenção no código fonte. "Store" não definida');
+	}
+	
+}
+
+
+
+
+/*var class_Name;
+Atualizar_Status();
+setInterval(Atualizar_Status, 120000);
+
+function Atualizar_Status(){
+	
+	if (window.Store){
 		if(Store.Me.battery != undefined){
 			if (class_Name == undefined){
 				for (q=0;q<document.querySelectorAll("div").length;q++){
@@ -31,4 +73,6 @@ function Atualizar_Status(){
 		console.log('É necessário fazer manutenção no código fonte. "Store" não definida');
 	}
 	
-}
+}*/
+
+
