c413f2b1a7d568a422ddd65969959014f5a7f508
Suebersson Montalvão <43149447+Suebersson@users.noreply.github.com>
2021-12-31 22:06:07 -0300

    Update ExtrairMembrosGrupo.js

--- ExtrairMembrosGrupo.js
+++ ExtrairMembrosGrupo.js
@@ -1,8 +1,8 @@
 
-// ########################## Atualizado em 05/11/2020 ######################################################################
-// Versão do WhatsApp: 2.2045.15
+// ########################## Atualizado em 01/01/2022 ######################################################################
+// Versão do WhatsApp: 2.2147.16
 
-var ListaContatos = new Array(), Nacional, num, j = 0, chats;
+var ListaContatos = new Array(), Nacional, num, j = 0, chatActive, chats;
 
 function ExtrairMembrosGrupo(){
 
@@ -10,7 +10,7 @@ function ExtrairMembrosGrupo(){
 
 	if (window.Store){
 		//Ler o grupo ativo, ler os contatos do grupo e imprimir
-		var chatActive = Store.Chat.active();
+		chatActive = Store.Chat.active();
 		
 		if(chatActive.__x_isGroup == true){
 			
@@ -34,76 +34,65 @@ function ExtrairMembrosGrupo(){
 						No: "Não"
 					},
 				}).then((value) => {
-					switch (value) {
-						case "Yes":
-							Nacional = 1
-							get_members()
-							break;
-						case "No":
-							Nacional = 0
-							get_members()
-							break;
-						default:
-						//Não fazer nada
-					}
+					
+					value === 'yes' ? Nacional = 1 : Nacional = 0;
+					get_members();
+					
 				});
-
-				function get_members(){
-					for (q = 0; q < chats.length; q++){
-						
-						if(Store.Conn.__x_wid.user == chats[q].__x_id.user){
-							//Excluir o número da conta logada que também participa do grupo
-						}else{
-							//console.log(chats[q].__x_id.user);
-							num = chats[q].__x_id.user
-							
-							if(Nacional == 1){
-								if(num.slice(0, 2) == "55"){//remover o código 55
-									ListaContatos[j] = num.slice(2, num.length)
-									j++
-								}
-							}else{//Capturar todos os números, nacionais e internacionais
-							
-								if(num.slice(0, 2) == "55"){//remover o código 55
-									ListaContatos[j] = num.slice(2, num.length)
-									j++
-								}else{
-									ListaContatos[j] = num
-									j++
-								}
-								
-							}
-						}
-					}
-					
-					var fileNameToSaveAs = prompt('Digite ou verifique o nome do grupo (Excluindo emojis):', chatActive.__x_formattedTitle) + '.txt'; //Nome do txt
-
-					if(fileNameToSaveAs != '' && fileNameToSaveAs != 'null.txt'){
-	
-						printTxt(ListaContatos, fileNameToSaveAs)
-						ListaContatos = new Array()
-					
-					}else{
-						alert("Nome do arquivo não inserido")
-					}
-	
-				}
 			
 			}else{
 				alert('Nesse grupo não à participantes suficiente para extrair os contatos')
 			}
-		
-			
 		}else{
 			alert("O Chat selecionado não é um grupo")
 		}
-
 	}else{
 		console.warn('É necessário fazer manutenção no código fonte. "Store" não definida')
 	}
 
 }
 
+function get_members(){
+	
+	var myPhone = Store.Me.Conn.__x_wid.user;
+	
+	for (q = 0; q < chats.length; q++){
+		
+		if(myPhone != chats[q].__x_id.user){
+			//console.log(chats[q].__x_id.user);
+			num = chats[q].__x_id.user
+			
+			if(Nacional == 1){
+				if(num.slice(0, 2) == "55"){//remover o código 55
+					ListaContatos[j] = num.slice(2, num.length)
+					j++
+				}
+			}else{//Capturar todos os números, nacionais e internacionais
+			
+				if(num.slice(0, 2) == "55"){//remover o código 55
+					ListaContatos[j] = num.slice(2, num.length)
+					j++
+				}else{
+					ListaContatos[j] = num
+					j++
+				}
+				
+			}
+		}
+	}
+	
+	var fileNameToSaveAs = prompt('Digite ou verifique o nome do grupo (Excluindo emojis):', chatActive.__x_formattedTitle) + '.txt'; //Nome do txt
+
+	if(fileNameToSaveAs != '' && fileNameToSaveAs != 'null.txt'){
+
+		printTxt(ListaContatos, fileNameToSaveAs)
+		ListaContatos = new Array()
+	
+	}else{
+		alert("Nome do arquivo não inserido")
+	}
+
+}
+
+
 //Store.Chat.active().__x_groupMetadata.participants._models[0].__x_id.user.slice(0, 2);
-
-
